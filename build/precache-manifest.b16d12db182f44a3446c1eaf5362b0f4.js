79bf97fbf5f91597d1e538660ea5415b36ace04e
santiago_valenzuela <svalenzuela@nexura.com>
2026-01-26 11:33:43 -0500

    Correccion de rama master

---
+++ build/precache-manifest.b16d12db182f44a3446c1eaf5362b0f4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022a620401ad31b85d3b7b55f89185a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a73e01a0d63af07e9cb6",
+    "url": "/static/css/2.89725360.chunk.css"
+  },
+  {
+    "revision": "9f7282459535000ac440",
+    "url": "/static/css/main.8d7427b0.chunk.css"
+  },
+  {
+    "revision": "a73e01a0d63af07e9cb6",
+    "url": "/static/js/2.b7469cc7.chunk.js"
+  },
+  {
+    "revision": "aa05953c9566911d5924866b7674b86d",
+    "url": "/static/js/2.b7469cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4fdd9960d55a9f7500",
+    "url": "/static/js/3.76ee505c.chunk.js"
+  },
+  {
+    "revision": "e9a23d6a03691b1cee300a2b4f13917b",
+    "url": "/static/js/3.76ee505c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7282459535000ac440",
+    "url": "/static/js/main.c93f74ac.chunk.js"
+  },
+  {
+    "revision": "9320f50acd7542d2b4f4",
+    "url": "/static/js/runtime-main.e5176a25.js"
+  },
+  {
+    "revision": "0bd3827ce211051e300b1d97bfae08fb",
+    "url": "/static/media/beep.0bd3827c.mp3"
+  },
+  {
+    "revision": "ad11bb537a107bc12e9c9921c38a9a8e",
+    "url": "/static/media/classic_phone.ad11bb53.mp3"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/static/media/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "922fdfbc530774b1af6584ce8a912544",
+    "url": "/static/media/testenviosMasivos.922fdfbc.csv"
+  }
+]);
